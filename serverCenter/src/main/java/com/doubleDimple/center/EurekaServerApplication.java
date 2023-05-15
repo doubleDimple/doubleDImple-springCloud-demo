c58bd3f725eb9e1744c5a5459e92fbfed680afba
doubleDimple <lovele.cn@gmail.com>
2023-05-15 17:31:14 +0800

    日志

--- serverCenter/src/main/java/com/doubleDimple/center/EurekaServerApplication.java
+++ serverCenter/src/main/java/com/doubleDimple/center/EurekaServerApplication.java
@@ -1,11 +1,15 @@
 package com.doubleDimple.center;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
+@EnableAdminServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
